3e6da7600d76f61f40ab1434ad81e073e174075f
Aisha-Yusuf <ayshaofficiel@gmail.com>
2023-09-22 20:02:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Aisha Yusuf';
+const welcomeMessage = 'Welcome to Flatbook, currentUser' + 'Aisha Yusuf!';
+const excitedWelcomeMessage = 'Welcome to Flatbook, Aisha Yusuf!'.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
